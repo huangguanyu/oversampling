2963cc8399d47aa000006a39f8baac719c4b209e
huangguanyu@gmail.com <huangguanyu@gmail.com>
2018-05-03 21:51:33 -0400

    class file

---
+++ oversampling.py
@@ -0,0 +1,21 @@
+
+
+#this is an oversampling library for creating high resolution satellite imagesd
+#based on *** paper.
+
+import scipy.io
+import datetime
+import os.system
+
+
+class oversampling:
+
+	def _init_():
+		self.data =[]
+
+	version={"0.1", 
+	"built by Kang Sun and Guanyu Huang",
+	"based on XX paper"}
+
+
+
